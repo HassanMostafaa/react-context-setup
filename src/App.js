bd52dc5f70fe49b3e9c8a7e5fb2c432bcc08c6e7
HassanMostafaa <hassan.bmw2251@gmail.com>
2022-06-13 16:09:02 +0200

    uploadSetup

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
   return (
     <MyContext.Provider value={{ state, setState }}>
       <div className="App container">
-        <h1>Head</h1>
         <User />
       </div>
     </MyContext.Provider>
